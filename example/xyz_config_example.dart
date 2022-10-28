e487c0f1d209fb84ff487bb3e245c4dd142d4e67
Robert Mollentze <robmllze@gmail.com>
2022-10-28 13:45:43 +1100

    worked on translation capability

--- example/xyz_config_example.dart
+++ example/xyz_config_example.dart
@@ -1,9 +1,28 @@
 import 'package:xyz_config/xyz_config.dart';
-import 'dart:io' as io;
+import 'dart:io';
 
 Future<void> main() async {
-  final a = await XyzConfig.configFromYaml(() => io.File("config.yaml").readAsString());
-  print(a.config);
-  final b = await XyzConfig.configFromJsonc(() => io.File("config.jsonc").readAsString());
-  print(a.config);
+  //
+  //
+  //
+
+  final xyzConfigManager = await XyzConfigManager.create(() async {
+    final nodes = <XyzConfigNode>[];
+    for (final path in ["test.yaml"]) {
+      nodes.add(
+        XyzConfigNode(
+          path,
+          await XyzConfig.fromYaml(() => File(path).readAsString()),
+        ),
+      );
+    }
+    return nodes;
+  });
+
+  print("(=this_file_type)".tr(xyzConfigManager));
+
+  //final a = await XyzConfig.fromYaml(() => File("test.yaml").readAsString());
+  // print(a.config);
+  // final b = await XyzConfig.fromJsonc(() => io.File("test.jsonc").readAsString());
+  // print(b.config);
 }
--- lib/src/xyz_config_base.dart
+++ lib/src/xyz_config_base.dart
@@ -1,56 +1,206 @@
 import 'dart:convert';
-
 import 'package:yaml/yaml.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
+abstract class XyzLocaleRef {
+  final String countryCode;
+  final String languageCode;
+  final String localeCode;
+  final String fileType;
+  const XyzLocaleRef(
+    this.countryCode,
+    this.languageCode,
+    this.fileType,
+  ) : localeCode = "$countryCode-$languageCode";
+  @override
+  String toString() {
+    return "$localeCode.$fileType".toLowerCase();
+  }
+}
+
+class XyzLocaleRefJson extends XyzLocaleRef {
+  const XyzLocaleRefJson(String countryCode, String languageCode)
+      : super(countryCode, languageCode, "json");
+}
+
+class XyzLocaleRefJsonc extends XyzLocaleRef {
+  const XyzLocaleRefJsonc(String countryCode, String languageCode)
+      : super(countryCode, languageCode, "jsonc");
+}
+
+class XyzLocaleRefYaml extends XyzLocaleRef {
+  const XyzLocaleRefYaml(String countryCode, String languageCode)
+      : super(countryCode, languageCode, "yaml");
+}
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+class XyzConfigNode {
+  final String path;
+  final XyzConfig config;
+  const XyzConfigNode(this.path, this.config);
+}
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+class XyzConfigManager {
+  //
+  //
+  //
+
+  late final XyzConfigNode _current;
+  final List<XyzConfigNode> _configs;
+  final String _sOpen;
+  final String _sClose;
+
+  //
+  //
+  //
+
+  XyzConfigManager._(this._configs, this._sOpen, this._sClose) {
+    assert(_configs.isNotEmpty);
+    _current = _configs.last;
+  }
+
+  //
+  //
+  //
+
+  static Future<XyzConfigManager> create(
+    Future<List<XyzConfigNode>> Function() loader, {
+    String sOpen = r"\(\=",
+    String sClose = r"\)",
+  }) async {
+    final configs = await loader();
+    return XyzConfigManager._(configs, sOpen, sClose);
+  }
+
+  //
+  //
+  //
+
+  void setCurrentByFilePath(String filePath) {
+    _current = _configs.firstWhere((final l) => l.path == filePath);
+  }
+
+  void setCurrentByLocaleRef(XyzLocaleRef locale) {
+    _current = _configs.firstWhere((final l) => l.path == locale.toString());
+  }
+
+  //
+  //
+  //
+
+  String _translate(String src, [Map<dynamic, dynamic> args = const {}]) {
+    try {
+      final args0 = _configs.firstWhere((final l) => l.path == _current.path).config._config;
+      return _handle(src, {...args0, ...args}, _sOpen, _sClose).toString();
+    } catch (_) {}
+    return "???";
+  }
+}
+
+extension Translate on String {
+  String tr(XyzConfigManager configManager, [Map<dynamic, dynamic> args = const {}]) {
+    return configManager._translate(this, args);
+  }
+}
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+dynamic _handle(
+  String input,
+  Map<dynamic, dynamic> handles,
+  String sOpen,
+  String sClose,
+) {
+  var copy = input;
+  for (final entry in handles.entries) {
+    final k = entry.key;
+    final v = entry.value;
+    final source = "$sOpen$k$sClose";
+    if (input == source) return v;
+    final expression = RegExp(source, caseSensitive: false);
+    copy = copy.replaceAll(expression, v.toString());
+  }
+  return copy;
+}
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
 class XyzConfig {
+  //
+  //
+  //
+
   final _config = <dynamic, dynamic>{};
   Map<dynamic, dynamic> get config => _config;
+  final String _sOpen;
+  final String _sClose;
 
-  XyzConfig._();
+  //
+  //
+  //
 
-  static Future<XyzConfig> configFromYaml(
+  XyzConfig._(this._sOpen, this._sClose);
+
+  //
+  //
+  //
+
+  static Future<XyzConfig> fromYaml(
     Future<String> Function() loader, {
     Uri? sourceUrl,
+    String sOpen = r"\(\=",
+    String sClose = r"\)",
   }) async {
-    final instance = XyzConfig._();
-    await instance._configFromYaml(loader, sourceUrl: sourceUrl);
+    final instance = XyzConfig._(sOpen, sClose);
+    await instance._fromYaml(loader);
     return instance;
   }
 
-  Future<void> _configFromYaml(
-    Future<String> Function() loader, {
-    Uri? sourceUrl,
-  }) async {
+  Future<void> _fromYaml(Future<String> Function() loader) async {
     final src = await loader();
-    final yaml = loadYaml(src, sourceUrl: sourceUrl);
+    final yaml = loadYaml(src);
     if (yaml is YamlMap) {
       _parseYaml(yaml);
       _replace(_config);
     }
   }
 
-  static Future<XyzConfig> configFromJsonc(Future<String> Function() loader) async {
-    final instance = XyzConfig._();
-    await instance._configFromJsonc(loader);
-    return instance;
-  }
-
-  Future<void> _configFromJsonc(Future<String> Function() loader) async {
-    final src = await loader();
-    _parseJson(src);
-    _replace(_config);
-  }
-
   void _parseYaml(YamlMap input) {
     _parse<YamlList, YamlMap>(input, _config);
   }
 
-  void _parseJson(String input) {
+  //
+  //
+  //
+
+  static Future<XyzConfig> fromJsonc(
+    Future<String> Function() loader, {
+    String sOpen = r"\(\=",
+    String sClose = r"\)",
+  }) async {
+    final instance = XyzConfig._(sOpen, sClose);
+    await instance._fromJsonc(loader);
+    return instance;
+  }
+
+  Future<void> _fromJsonc(Future<String> Function() loader) async {
+    final src = await loader();
+    _parseJsonc(src);
+    _replace(_config);
+  }
+
+  void _parseJsonc(String input) {
     final decoded = jsonDecode(input.replaceAll(RegExp(r"\/\/.*"), ""));
     _parse<List, Map>(decoded, _config);
   }
+
+  //
+  //
+  //
 
   void _parse<TList, TMap>(TMap input, Map<dynamic, dynamic> global) {
     final local = <dynamic, dynamic>{};
@@ -78,7 +228,7 @@ class XyzConfig {
           r[k] = py;
         }
       } else if (input is String) {
-        r = _handle(input, local);
+        r = _handle(input, local, _sOpen, _sClose);
       } else {
         r = input;
       }
@@ -87,6 +237,10 @@ class XyzConfig {
 
     $parse(input) as Map<dynamic, dynamic>;
   }
+
+  //
+  //
+  //
 
   void _replace(Map<dynamic, dynamic> global) {
     dynamic $replace<T>(dynamic key, dynamic value) {
@@ -105,7 +259,7 @@ class XyzConfig {
         }
       }
       if (value is String) {
-        r = _handle(value, global);
+        r = _handle(value, global, _sOpen, _sClose);
       } else {
         r = value;
       }
@@ -119,44 +273,3 @@ class XyzConfig {
     $replace(null, global /*Map.of(global)*/);
   }
 }
-
-dynamic _handle(String input, Map<dynamic, dynamic> handles) {
-  var copy = input;
-  for (final entry in handles.entries) {
-    final k = entry.key;
-    final v = entry.value;
-    final source = "{$k}";
-    if (input == source) return v;
-    final expression = RegExp(source, caseSensitive: false);
-    copy = copy.replaceAll(expression, v.toString());
-  }
-  return copy;
-}
-
-String _translate(
-  String src,
-  String locale,
-  Map<String, XyzConfig> configs,
-) {
-  final handles = configs[locale]?.config;
-  if (handles?.isNotEmpty == true) {
-    return _handle(src, handles!).toString();
-  }
-  return "???";
-}
-
-
-
-// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-
-// Future<String> getGameData(String src) async {
-//   final response = await HttpRequest.getString(
-//     "$src.yaml",
-//     onProgress: (_) {
-//       //
-//     },
-//   );
-//   return response;
-// }
-
-
